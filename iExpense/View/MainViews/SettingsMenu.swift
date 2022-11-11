f42ce10dbbec29a872574e26b5c6e30eaab2b4d8
Marcus Arkan <marcus@swlend.com>
2022-11-11 06:58:01 -0500

    starting settingsMenu file and updated previous files

---
+++ iExpense/View/MainViews/SettingsMenu.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsMenu.swift
+//  iExpense
+//
+//  Created by Marcus Arkan on 11/11/22.
+//
+
+import SwiftUI
+
+struct SettingsMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsMenu()
+    }
+}
